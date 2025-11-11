663c9fda490868ec9bec42c0c1741d9e36691c94
Tsunaoka013 <200327516+Tsunaoka013@users.noreply.github.com>
2025-11-11 01:05:38 +0000

    chapter0

---
+++ code02.java
@@ -0,0 +1,9 @@
+public class code02 {
+    public static void main(String[] args) {
+        System.out.println(31 + 31);
+        System.out.println(35 - 10);
+        System.out.println(-5 * 2);
+        System.out.println(6 * 6 * 3.14);
+        System.out.println("solve=" + 64);
+    }
+}
